2e2ea434b9519ca5b457c23676f8704d16e6ab96
cryptozeny <cryptozeny@gmail.com>
2018-06-22 02:58:13 +0900

    fix: cmake

---
+++ bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/scrypt-arm.S" "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/scrypt-arm.S.o"
+  "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/scrypt-x64.S" "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/scrypt-x64.S.o"
+  "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/scrypt-x86.S" "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/scrypt-x86.S.o"
+  "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/sha2-arm.S" "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/sha2-arm.S.o"
+  "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/sha2-x64.S" "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/sha2-x64.S.o"
+  "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/sha2-x86.S" "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/sha2-x86.S.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "__STDC_CONSTANT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  "__linux"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../distribution/zlib/include"
+  "../distribution/curl/include"
+  "../distribution/jansson/include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/libcpuminer.c" "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/libcpuminer.c.o"
+  "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/scrypt.c" "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/scrypt.c.o"
+  "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/sha2.c" "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/sha2.c.o"
+  "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/util.c" "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/util.c.o"
+  "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/yescrypt.c" "/home/ak/git/CPUMINER/ANDROID/BitZenyAndroidMiningLibrary/bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/yescrypt.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "__STDC_CONSTANT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  "__linux"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../distribution/zlib/include"
+  "../distribution/curl/include"
+  "../distribution/jansson/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ bitzenymininglibrary/src/main/cpp/libcpuminer/CMakeFiles/cpuminer.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpuminer.dir/scrypt.c.o"
+  "CMakeFiles/cpuminer.dir/scrypt-arm.S.o"
+  "CMakeFiles/cpuminer.dir/scrypt-x64.S.o"
+  "CMakeFiles/cpuminer.dir/scrypt-x86.S.o"
+  "CMakeFiles/cpuminer.dir/sha2.c.o"
+  "CMakeFiles/cpuminer.dir/sha2-arm.S.o"
+  "CMakeFiles/cpuminer.dir/sha2-x64.S.o"
+  "CMakeFiles/cpuminer.dir/sha2-x86.S.o"
+  "CMakeFiles/cpuminer.dir/util.c.o"
+  "CMakeFiles/cpuminer.dir/yescrypt.c.o"
+  "CMakeFiles/cpuminer.dir/libcpuminer.c.o"
+  "libcpuminer.pdb"
+  "libcpuminer.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/cpuminer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
